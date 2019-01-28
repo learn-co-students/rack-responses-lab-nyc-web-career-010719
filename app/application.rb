e23e0139e15154dec8833ebd8c6b5f63cdc0ff87
powell956 <powell956@gmail.com>
2019-01-28 13:30:52 -0500

    refactored to include evening greeting

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.now.hour < 12
+    if 5 < Time.now.hour < 12
       resp.write "Good Morning!"
-    else
+    elseif 12 < Time.now.hour < 17
       resp.write "Good Afternoon!"
+    elseif 17 < Time.now.hour || Time.now.hour < 5
+      resp.write "Good Evening!"
     end
 
     resp.finish
